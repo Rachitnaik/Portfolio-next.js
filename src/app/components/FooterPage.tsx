4a09dd91ffded97e60515a647756ae16b3a2a6df
Rachit Gaunkar <rachit.gaunkar@zapcg.com>
2024-11-25 15:42:32 +0530

    hot fix of arrows

--- src/app/components/FooterPage.tsx
+++ src/app/components/FooterPage.tsx
@@ -1,18 +1,28 @@
-import { Image } from "@nextui-org/react";
+import React from "react";
+import { useMediaQuery } from "@mui/material";
+import CallReceivedIcon from "@mui/icons-material/CallReceived";
+import CallMadeIcon from "@mui/icons-material/CallMade";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "./FooterPage.css";
-import "../globals.css";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
 
 const FooterPage: React.FC = () => {
+    // Use media queries to determine screen size
+    const isMobileOrTablet = useMediaQuery("(max-width: 1024px)");
+
     return (
         <>
-            <div className="footer-container poppins" >
-                <div className="footer-text" >
+            <div className="footer-container poppins">
+                <div className="footer-text">
                     <p className="footer-number poppins">4</p>
-                    <p className="footer-title poppins">Get in touch    </p>
-
+                    <p className="footer-title poppins">
+                        Get in touch{" "}
+                        {isMobileOrTablet ? (
+                            <CallReceivedIcon className="arrow" />
+                        ) : (
+                            <CallMadeIcon className="arrow" />
+                        )}
+                    </p>
                 </div>
 
                 <div className="footer-icons-wrapper">
@@ -20,26 +30,24 @@ const FooterPage: React.FC = () => {
                         <a href="https://github.com/Rachitnaik" target="_blank" rel="noopener noreferrer">
                             <GitHubIcon className="footer-icon" />
                         </a>
-                        <a href="https://www.linkedin.com/in/rachitnaikgaunkar/" target="_blank" rel="noopener noreferrer">
+                        <a
+                            href="https://www.linkedin.com/in/rachitnaikgaunkar/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             <LinkedInIcon className="footer-icon" />
                         </a>
                     </div>
-                    <div className="poppins footer-mail" > <p >Mail to : rachitgaunkar@gmail.com</p>
-
+                    <div className="poppins footer-mail">
+                        <p>Mail to : rachitgaunkar@gmail.com</p>
                     </div>
-
-
                 </div>
-
             </div>
             <div className="poppins devtext">
-                <p >Developed By :</p>
+                <p>Developed By :</p>
                 <p>Rachit Naik Gaunkar</p>
-
             </div>
-
         </>
-
     );
 };
 
